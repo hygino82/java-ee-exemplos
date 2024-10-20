838e470b7e08c212f96240eb4c23c94c252476f5
Adroaldo <hygino@msn.com>
2024-10-20 22:42:15 +0000

    Criando o EJB Local

---
+++ src/main/java/be/ejb/EjbConversorTemperatura.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package be.ejb;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author default
+ */
+@Stateless
+public class EjbConversorTemperatura implements EjbConversorTemperaturaLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
